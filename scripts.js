710781b16daf52aad5f204965f9ea59a559d2a55
francienevaz <fran.vfraga@gmail.com>
2023-02-19 15:36:58 -0300

    finish

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
-const passField = document.querySelector(".password");
-const showBtn = document.querySelector(".show-password i");
+const passField = document.querySelectorAll(".password");
+const showBtn = document.querySelectorAll(".show-password i");
 
-showBtn.onclick = () => {
-    if (passField.type === "password") {
-        passField.type = "text";
-        showBtn.classList.add("hide-btn");
+showBtn[0].onclick = () => {
+    if (passField[0].type === "password") {
+        passField[0].type = "text";
+        showBtn[0].classList.add("hide-btn");
     } else {
-        passField.type = "password";
-        showBtn.classList.remove("hide-btn");
+        passField[0].type = "password";
+        showBtn[0].classList.remove("hide-btn");
+    }
+}
+
+showBtn[1].onclick = () => {
+    if (passField[1].type === "password") {
+        passField[1].type = "text";
+        showBtn[1].classList.add("hide-btn");
+    } else {
+        passField[1].type = "password";
+        showBtn[1].classList.remove("hide-btn");
     }
 }
